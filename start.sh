a68caae3f7c9983e4b86ab351de454ec708ed7f7
root <hrunker@gmail.com>
2019-04-25 23:19:02 +0800

    add config

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-export KAFKA_BROKER_LIST=
+export KAFKA_BROKER_LIST=10.10.26.24:9092
 export KAFKA_TOPIC=
 export KAFKA_COMPRESSION=
 export KAFKA_BATCH_NUM_MESSAGES
 export SERIALIZATION_FORMAT=
-export PORT=
+export PORT=8009
 export LOG_LEVEL=
 export GIN_MODE=
+
+./prometheus-kafka-adapter
